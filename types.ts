a16aa25d36b1d237dc463f561f1d01ffa43c53d2
Robin <daorob@gmail.com>
2020-01-08 21:10:24 -0600

    Add example of types notes.

---
+++ types.ts
@@ -0,0 +1,23 @@
+// Example 1 
+const today = new Date();
+// getMonth method exists
+today.getMonth();
+// rawr method does not exist
+today.rawr;
+
+// Example 2
+const person = {
+    age: 20
+};
+// age key exists
+person.age
+// rawr key doesn't not exist
+person.rawr
+
+// Example 3
+class Color {
+
+};
+const red = new Color();
+// no properties associated with this class of Color
+red.
