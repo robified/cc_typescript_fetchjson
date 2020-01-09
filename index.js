9f2308887374b59f74a253bc87c4b1d36d9fa550
Robin <daorob@gmail.com>
2020-01-08 20:48:21 -0600

    Apply Todo interface.

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
-"use strict";
+'use strict';
 exports.__esModule = true;
-var axios_1 = require("axios");
+var axios_1 = require('axios');
 var url = 'http://jsonplaceholder.typicode.com/todos/1';
 // this is asynchronous, so this will receive a promise
-axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+axios_1['default'].get(url).then(function(response) {
+    // console.log(response.data);
+
+    const todo = response.data;
+
+    const ID = todo.ID;
+    const title = todo.Title;
+    const finished = todo.finished;
+
+    console.log(`
+        The Todo with ID: ${ID}
+        Has a title of: ${title}
+        Is it finished: ${finished}
+    `);
 });
--- index.ts
+++ index.ts
@@ -2,7 +2,28 @@ import axios from 'axios';
 
 const url = 'http://jsonplaceholder.typicode.com/todos/1';
 
+// Interface is used to define the structure of an object
+interface Todo {
+    id: number;
+    title: string;
+    completed: boolean;
+}
+
 // this is asynchronous, so this will receive a promise
 axios.get(url).then(response => {
-    console.log(response.data);
+    // Reponse.data has properties of: id, title, completed
+    // console.log(response.data);
+
+    // apply Todo interface
+    const todo = response.data as Todo;
+
+    const id = todo.id;
+    const title = todo.title;
+    const completed = todo.completed;
+
+    console.log(`
+        The Todo with ID: ${id}
+        Has a title of: ${title}
+        Is it finished: ${completed}
+    `);
 });
